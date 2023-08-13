089562f66173ae14322c25e7a6b58f611759c2d5
danielmosilva <daniel.mosilva@gmail.com>
2023-08-13 16:39:35 -0300

    Renomeando classe que trata erros

--- src/main/java/med/vol/api/infra/ExceptionHandler.java
+++ src/main/java/med/vol/api/infra/ExceptionHandler.java
@@ -4,14 +4,12 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 
 @RestControllerAdvice
-public class TratadorDeErros {
+public class ExceptionHandler {
 
     private record DadosErroValidacao(String campo, String Mensagem) {
         public DadosErroValidacao(FieldError error){
@@ -20,12 +18,12 @@ public class TratadorDeErros {
 
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> tratarErro404() {
         return ResponseEntity.notFound().build();
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors().stream().map(DadosErroValidacao::new).toList();
         return ResponseEntity.badRequest().body(erros);
