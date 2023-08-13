f9bf49fab75afa47a3634050bf68b017927ca33d
danielmosilva <daniel.mosilva@gmail.com>
2023-08-13 16:28:58 -0300

    Tratando erro MethodArgumentNotValidException

--- src/main/java/med/vol/api/infra/TratadorDeErros.java
+++ src/main/java/med/vol/api/infra/TratadorDeErros.java
@@ -2,15 +2,33 @@ package med.vol.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
 
+    private record DadosErroValidacao(String campo, String Mensagem) {
+        public DadosErroValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors().stream().map(DadosErroValidacao::new).toList();
+        return ResponseEntity.badRequest().body(erros);
     }
 
 }
