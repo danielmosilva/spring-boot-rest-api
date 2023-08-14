6bd937950668b83467c37d6dd1b71ba47670cdae
danielmosilva <daniel.mosilva@gmail.com>
2023-08-14 01:37:35 -0300

    Adicionando alguns tratamentos de erro

--- src/main/java/med/vol/api/infra/exception/TratadorDeErro.java
+++ src/main/java/med/vol/api/infra/exception/TratadorDeErro.java
@@ -1,32 +1,68 @@
 package med.vol.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
+import javax.security.sasl.AuthenticationException;
 import java.util.List;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class TratadorDeErro {
 
     private record DadosErroValidacao(String campo, String Mensagem) {
-        public DadosErroValidacao(FieldError error){
+        public DadosErroValidacao(FieldError error) {
             this(error.getField(), error.getDefaultMessage());
         }
 
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(EntityNotFoundException.class)
+    private record DadosErroGenerico(String mensagem){
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> tratarErro404() {
         return ResponseEntity.notFound().build();
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors().stream().map(DadosErroValidacao::new).toList();
         return ResponseEntity.badRequest().body(erros);
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(new DadosErroGenerico(ex.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new DadosErroGenerico("Credenciais inválidas"));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new DadosErroGenerico("Falha na autenticação"));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new DadosErroGenerico("Acesso negado"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new DadosErroGenerico(ex.getLocalizedMessage()));
+    }
+
 
 }
