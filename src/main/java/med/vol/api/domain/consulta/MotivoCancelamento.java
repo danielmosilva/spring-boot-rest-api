0e64d297935a8e762e6091d089a11b6c4034c677
danielmosilva <daniel.mosilva@gmail.com>
2023-08-14 04:06:46 -0300

    Inserindo validaçoes de agendamento

--- src/main/java/med/vol/api/domain/consulta/MotivoCancelamento.java
+++ src/main/java/med/vol/api/domain/consulta/MotivoCancelamento.java
@@ -2,7 +2,7 @@ package med.vol.api.domain.consulta;
 
 public enum MotivoCancelamento {
 
-        PACIENTE_DESISTIU,
+    PACIENTE_DESISTIU,
     MEDICO_CANCELOU,
     OUTROS;
 
---
+++ src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,21 @@
+package med.vol.api.domain.consulta.validacoes;
+
+import med.vol.api.domain.ValidacaoException;
+import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisDoFechamentoDaClinica = dataConsulta.getHour() > 18;
+
+        if (domingo || antesDaAberturaDaClinica || depoisDoFechamentoDaClinica)
+            throw  new ValidacaoException("Consulta fora do horário de funcionamento da clinica");
+    }
+
+}
