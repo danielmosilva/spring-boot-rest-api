24a843d0947fcfd315341c43c6b4b88b1360def5
danielmosilva <daniel.mosilva@gmail.com>
2023-08-14 03:34:23 -0300

    Implementando regras de negocio para agendamento de consultas

--- src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,7 @@
 package med.vol.api.domain.consulta;
 
+import med.vol.api.domain.ValidacaoException;
+import med.vol.api.domain.medico.Medico;
 import med.vol.api.domain.medico.MedicoRepository;
 import med.vol.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +21,33 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados){
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico()))
+            throw new ValidacaoException("Não foi possivel efetuar o agendamento pois o id do médico não existe");
+
+        if(!pacienteRepository.existsById(dados.idPaciente()))
+            throw new ValidacaoException("Não foi possivel efetuar o agendamento pois o id do paciente não existe");
+
+
+        var medico = escolherMedico(dados);
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var consulta = new Consulta(null, medico, paciente, dados.data());
 
 
 
         consultaRepository.save(consulta);
     }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+
+        if (dados.idMedico() != null)
+            return medicoRepository.getReferenceById(dados.idMedico());
+
+        if (dados.especialidade() == null)
+            throw new ValidacaoException("Especialidade é obrigatoria quando medico nao e selecionado");
+
+        return medicoRepository.escolherMedicoLivreNaData(dados.especialidade(), dados.data());
+
+    }
+
 
 }
