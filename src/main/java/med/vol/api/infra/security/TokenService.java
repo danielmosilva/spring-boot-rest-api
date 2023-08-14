2b80341958fd232842de103aacfbc8dd2a848b2b
danielmosilva <daniel.mosilva@gmail.com>
2023-08-13 23:28:41 -0300

    Incluindo verificação do token

--- src/main/java/med/vol/api/infra/security/TokenService.java
+++ src/main/java/med/vol/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.vol.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.vol.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,24 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT)
+    {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("spring-boot-rest-api")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+
+            throw new RuntimeException("Token JWT inválido ou expirado.");
+
         }
     }
 
